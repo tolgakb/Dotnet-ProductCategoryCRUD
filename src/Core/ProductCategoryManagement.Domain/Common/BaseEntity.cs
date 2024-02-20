d146718c94f7c01831199a60d1589db1cde8c9fe
tolgakb <tolgakbaahmet@gmail.com>
2024-02-20 16:01:06 +0000

    Add project files.

---
+++ src/Core/ProductCategoryManagement.Domain/Common/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCategoryManagement.Domain.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public BaseEntity()
+        {
+            CreatedDate = DateTime.UtcNow;
+        }
+    }
+}
---
+++ src/Infrastructure/ProductCategoryManagement.Persistence/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCategoryManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCategoryManagement.Persistence.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
